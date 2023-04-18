2f4e5b7635162ea2a2b34a5d6e7fe31a33bc98a0
Patryk Kozłowski <patryk.kozlowski@wercom.pl>
2023-04-18 14:16:52 +0200

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Services.SuperHeroService;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly ISuperHeroService _superHeroService;
+        public SuperHeroController(ISuperHeroService superHeroService) 
+        {
+            _superHeroService = superHeroService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            return await _superHeroService.GetAllHeroes();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingleHero(int id)
+        {
+            var result = await _superHeroService.GetSingleHero(id);
+            if (result is null) { return NotFound("Hero not found"); }
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddHero([FromBody]SuperHero hero)
+        {
+            var result = await _superHeroService.AddHero(hero);
+            if (result is null) { return NotFound("Hero not found"); }
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateHero(int id, SuperHero request)
+        {
+            var result = await _superHeroService.UpdateHero(id, request);
+            if (result is null) { return NotFound("Hero not found"); }
+            return Ok(result);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteHero(int id)
+        {
+            var result = await _superHeroService.DeleteHero(id);
+            if (result is null) { return NotFound("Hero not found"); }
+            return Ok(result);
+        }
+    }
+}
---
+++ SuperHeroAPI/Program.cs
@@ -0,0 +1,35 @@
+global using SuperHeroAPI.Models;
+global using SuperHeroAPI.Data;
+using SuperHeroAPI.Services.SuperHeroService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registered service
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+
+// Registered database
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
